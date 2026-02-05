fe21f12ac334478ac0ef71a77fa38ee505719036
twiggle <68298680+the-wiggler@users.noreply.github.com>
2026-02-05 15:31:55 -0500

    added some structs

---
+++ bridge/bridge.ino
@@ -0,0 +1,21 @@
+typedef struct {
+  bool boat_detected,
+  bool bridge_motor_activated,
+  bool bridge_raised
+} bridge_properties_t;
+
+#define MAIN_BRIDGE_MOTOR_PIN 1
+#define SECONDARY_BRIDGE_MOTOR_PIN 2
+#define BRIDGE_BOAT_SENSOR_PIN 3
+
+// setup code for the bridge
+void setup() {
+  // initialize the sensors and variables
+
+}
+
+// program loop for the bridge -- this runs infinitely
+void loop() {
+  // run the loop and such
+  
+}
--- car/car.ino
+++ car/car.ino
@@ -1,3 +1,11 @@
+typedef struct {
+  bool bridge_sensor_activated,
+  bool motor_running
+} sensor_properties_t;
+
+#define BRIDGE_SENSOR_PIN 1
+#define MOTOR_R_PIN 2
+#define MOTOR_L_PIN 3
 
 // setup code for the car
 void setup() {
