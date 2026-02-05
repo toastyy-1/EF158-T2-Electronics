a905d03587dd63e2ad5ea48c7bd7597a59d616f2
twiggle <68298680+the-wiggler@users.noreply.github.com>
2026-02-05 15:22:38 -0500

    added files

---
+++ bridge.ino
@@ -0,0 +1,12 @@
+
+// setup code for the bridge
+void setup() {
+  // initialize the sensors and variables
+
+}
+
+// program loop for the bridge -- this runs infinitely
+void loop() {
+  // run the loop and such
+  
+}
---
+++ car.ino
@@ -0,0 +1,12 @@
+
+// setup code for the car
+void setup() {
+  // initialize the sensors and variables
+
+}
+
+// program loop for the car -- this runs infinitely
+void loop() {
+  // run the loop and such
+  
+}
