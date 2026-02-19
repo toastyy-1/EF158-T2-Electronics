800df407f0757442578e6ea97a526a24f98c6071
twiggle <68298680+the-wiggler@users.noreply.github.com>
2026-02-19 15:39:38 -0500

    added bridge logic

--- bridge/bridge.ino
+++ bridge/bridge.ino
@@ -1,21 +1,45 @@
-typedef struct {
-  bool boat_detected,
-  bool bridge_motor_activated,
-  bool bridge_raised
-} bridge_properties_t;
+#define SONAR_TRIG_PIN 9
+#define SONAR_ECHO_PIN 10
+#define BUILTIN_LED_PIN 13
 
-#define MAIN_BRIDGE_MOTOR_PIN 1
-#define SECONDARY_BRIDGE_MOTOR_PIN 2
-#define BRIDGE_BOAT_SENSOR_PIN 3
-
-// setup code for the bridge
 void setup() {
-  // initialize the sensors and variables
-  
+  Serial.begin(9600);
+  pinMode(SONAR_TRIG_PIN, OUTPUT);
+  pinMode(SONAR_ECHO_PIN, INPUT);
+  pinMode(BUILTIN_LED_PIN, OUTPUT);
 }
 
-// program loop for the bridge -- this runs infinitely
+// @return returns distance in centimeters
+float get_distance() {
+  digitalWrite(SONAR_TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(SONAR_TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(SONAR_TRIG_PIN, LOW);
+
+  long duration = pulseIn(SONAR_ECHO_PIN, HIGH);
+  return duration * 0.034 / 2;
+}
+
+// start of control loop
+bool bridge_pulley_on = false;
 void loop() {
-  // run the loop and such
-  
+
+  float distance = get_distance();
+
+  if (distance < 10.0) {
+    digitalWrite(BUILTIN_LED_PIN, HIGH);
+  }
+  else {
+    digitalWrite(BUILTIN_LED_PIN, LOW);
+  }
+
+  if (bridge_pulley_on) {
+
+  }
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(10);
 }
